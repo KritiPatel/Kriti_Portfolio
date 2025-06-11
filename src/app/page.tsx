5fff0b4ffbcac700dbd95a86305c40b303613e9b
Kriti-we <72915842+Kriti-we@users.noreply.github.com>
2025-06-11 09:15:50 +0530

    feat : temp push

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+export default function Home() {
+  return (
+    <div>
+      <div style={{ flexDirection: 'row', justifyContent: 'flex-end' }}>
+        <div className="bg-slate-400">hello</div>
+        <div className="text-4xl">hellssswwo</div>
+      </div>
+    </div>
+  );
+}
